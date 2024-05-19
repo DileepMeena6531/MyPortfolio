47f8808a9ebb079a3ce2bb9d8a11575df0a31af5
unknown <2003dileepmeena@gmail.com>
2024-05-19 17:28:30 +0530

    my portfolio

---
+++ index.js
@@ -0,0 +1,16 @@
+var typed=new Typed(".text",{
+    strings:["Fronted Developer","Backend Developer","Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbar = document.querySelector('.navbar');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('open');
+    navbar.classList.toggle('open');
+});
